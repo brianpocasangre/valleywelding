a2808656913187f4b02dc0de5b78694d67cd0c85
brianpocasangre <brianpocasangre1@gmail.com>
2024-05-03 23:53:14 -0700

    add services, testimonial, and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./index.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        battleShipGrey: '#9A998C',
+        ashGrey: '#A2A392',
+        timberwolfGrey: '#D5D0CD',
+        timberwolfGreyDark: '#D4CDC3',
+        cosmicLatte: '#F8F4E3',
+        blueBtn: '#003b6d',
+        hoverBtn: '#6699CC',
+      },
+    },
   },
   plugins: [],
-}
-
+};
